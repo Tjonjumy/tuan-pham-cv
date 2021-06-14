65fdd030a294a0643ce8790c213f8c0af361bc38
tuan pham <tjonjumy@gmail.com>
2021-06-14 11:00:51 +0700

    update cv

--- myCV.js
+++ myCV.js
@@ -1,7 +1,6 @@
 // Write text
 const siteName = document.querySelector('.site-name h1');
 const siteslogan = document.querySelector('.site-slogan h3');
-const name = "I'm Tuan Pham";
 let idx = 0;
 function writeName(txt, selector) {
   let text = txt.slice(0, idx);
@@ -24,14 +23,14 @@ function loadWriteName() {
 }
 
 //Make Circle text
-function circularText(txt, radius, classIndex) {
+function circularText(txt, height, classIndex) {
   txt = txt.split(""),
     classIndex = document.getElementsByClassName("circTxt")[classIndex];
   var deg = 360 / txt.length,
     origin = -90;
 
   txt.forEach((ea) => {
-    ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
+    ea = `<p style='height:${height}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
     classIndex.innerHTML += ea;
     origin += deg;
   });
